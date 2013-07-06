4a8024ee9bc273d71f1a530dbbc68b80844c3268
Maili Porh <***infinite recursion in variable git_email***>
2013-07-06 23:01:01 +0300

    summary changes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Om::Application.config.secret_token = 'ae6a218c28cf9036c5bbadad5b2bed14d91fa931483bf3fdd9467613ffac5b0786745979b6e87ff1fe380983250127ea2a192d7777b13a4c660a74ad9647c2d7'
